8a82360cc0b8c96e68b11babc41eb3140f9238e7
Raghuvamsi Ananthu <raghuvamsiarv@gmail.com>
2021-06-24 06:45:58 +0530

    update small

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-	$serverName = "localhost";
-	$userName = "root";
-	$password = "";
-	$dbname = "raghuvamsi";
+	$serverName = "eu-cdbr-west-01.cleardb.com";
+	$userName = "b9066ff68b8732";
+	$password = "e0622d0c";
+	$dbname = "heroku_676455dc86be3ba";
 
 	$conn = mysqli_connect($serverName, $userName, $password, $dbname);
 	/*$conn = mysqli_connect('shareddb-x.hosting.stackcp.net','shivangi','12345678@','malyaraaj-313537e38d');*/
